25cfa2ffaa90211c3e05fc2b96e5cea8eaa294a6
markdl-dev <mdl.softwareengineer@gmail.com>
2021-03-30 03:44:01 +0800

    -updated coinsmarketstruct -changed type to pointer, added omitempty -client users now can determine nil or empty optional value

--- coingecko/coins.go
+++ coingecko/coins.go
@@ -18,40 +18,40 @@ type CoinsMarket []CoinsMarketStruct
 
 // CoinsMarketStruct is the coins market result
 type CoinsMarketStruct struct {
-	ID                                  string  `json:"id"`
-	Symbol                              string  `json:"symbol"`
-	Name                                string  `json:"name"`
-	CoinImage                           string  `json:"image"`
-	CurrentPrice                        float64 `json:"current_price"`
-	MarketCap                           float64 `json:"market_cap"`
-	MarketCapRank                       uint16  `json:"market_cap_rank"`
-	FullyDilutedValuation               float64 `json:"fully_diluted_valuation"`
-	TotalVolume                         float64 `json:"total_volume"`
-	High24H                             float64 `json:"high_24h"`
-	Low24H                              float64 `json:"low_24h"`
-	PriceChange24H                      float64 `json:"price_change_24h"`
-	PriceChangePercentage24H            float64 `json:"price_change_percentage_24h"`
-	MarketCapChange24H                  float64 `json:"market_cap_change_24h"`
-	MarketCapChangePercentage24H        float64 `json:"market_cap_change_percentage_24h"`
-	CirculatingSupply                   float64 `json:"circulating_supply"`
-	TotalSupply                         float64 `json:"total_supply"`
-	MaxSupply                           float64 `json:"max_supply"`
-	ATH                                 float64 `json:"ath"`
-	ATHChangePercentage                 float64 `json:"ath_change_percentage"`
-	ATHDate                             string  `json:"ath_date"`
-	ATL                                 float64 `json:"atl"`
-	ATLChangePercentage                 float64 `json:"atl_change_percentage"`
-	ATLDate                             string  `json:"atl_date"`
-	ROI                                 ROI     `json:"roi"`
-	LastUpdated                         string  `json:"last_updated"`
-	PriceChangePercentage1hInCurrency   float64 `json:"price_change_percentage_1h_in_currency"`
-	PriceChangePercentage24hInCurrency  float64 `json:"price_change_percentage_24h_in_currency"`
-	PriceChangePercentage7DInCurrency   float64 `json:"price_change_percentage_7d_in_currency"`
-	PriceChangePercentage14DInCurrency  float64 `json:"price_change_percentage_14d_in_currency"`
-	PriceChangePercentage24DInCurrency  float64 `json:"price_change_percentage_24d_in_currency"`
-	PriceChangePercentage30DInCurrency  float64 `json:"price_change_percentage_30d_in_currency"`
-	PriceChangePercentage200dInCurrency float64 `json:"price_change_percentage_200d_in_currency"`
-	PriceChangePercentage1yInCurrency   float64 `json:"price_change_percentage_1y_in_currency"`
+	ID                                  string   `json:"id"`
+	Symbol                              string   `json:"symbol"`
+	Name                                string   `json:"name"`
+	CoinImage                           string   `json:"image"`
+	CurrentPrice                        float64  `json:"current_price"`
+	MarketCap                           float64  `json:"market_cap"`
+	MarketCapRank                       uint16   `json:"market_cap_rank"`
+	FullyDilutedValuation               float64  `json:"fully_diluted_valuation"`
+	TotalVolume                         float64  `json:"total_volume"`
+	High24H                             float64  `json:"high_24h"`
+	Low24H                              float64  `json:"low_24h"`
+	PriceChange24H                      float64  `json:"price_change_24h"`
+	PriceChangePercentage24H            float64  `json:"price_change_percentage_24h"`
+	MarketCapChange24H                  float64  `json:"market_cap_change_24h"`
+	MarketCapChangePercentage24H        float64  `json:"market_cap_change_percentage_24h"`
+	CirculatingSupply                   float64  `json:"circulating_supply"`
+	TotalSupply                         float64  `json:"total_supply"`
+	MaxSupply                           float64  `json:"max_supply"`
+	ATH                                 float64  `json:"ath"`
+	ATHChangePercentage                 float64  `json:"ath_change_percentage"`
+	ATHDate                             string   `json:"ath_date"`
+	ATL                                 float64  `json:"atl"`
+	ATLChangePercentage                 float64  `json:"atl_change_percentage"`
+	ATLDate                             string   `json:"atl_date"`
+	ROI                                 *ROI     `json:"roi,omitempty"`
+	LastUpdated                         string   `json:"last_updated"`
+	PriceChangePercentage1HInCurrency   *float64 `json:"price_change_percentage_1h_in_currency,omitempty"`
+	PriceChangePercentage24HInCurrency  *float64 `json:"price_change_percentage_24h_in_currency,omitempty"`
+	PriceChangePercentage7DInCurrency   *float64 `json:"price_change_percentage_7d_in_currency,omitempty"`
+	PriceChangePercentage14DInCurrency  *float64 `json:"price_change_percentage_14d_in_currency,omitempty"`
+	PriceChangePercentage24DInCurrency  *float64 `json:"price_change_percentage_24d_in_currency,omitempty"`
+	PriceChangePercentage30DInCurrency  *float64 `json:"price_change_percentage_30d_in_currency,omitempty"`
+	PriceChangePercentage200dInCurrency *float64 `json:"price_change_percentage_200d_in_currency,omitempty"`
+	PriceChangePercentage1yInCurrency   *float64 `json:"price_change_percentage_1y_in_currency,omitempty"`
 }
 
 // ROI is the roi result
@@ -85,7 +85,7 @@ func (s *CoinsService) GetMarketsWithContext(ctx context.Context, vsCurrency str
 	urlValues.Add("per_page", "100")
 	urlValues.Add("page", "1")
 	urlValues.Add("sparkline", "false")
-	urlValues.Add("price_change_percentage", "1h,24h,7d,14d,30d,200d,1y")
+	urlValues.Add("price_change_percentage", "1h,24h,7d,14d,30d")
 
 	u.RawQuery = urlValues.Encode()
 
